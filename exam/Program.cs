b72a389a92c66a7f10fba450c35353c83a11d315
kat <kat.timofeeva2020@yandex.ru>
2024-07-08 19:29:50 +0300

    написала программу создающую массив строк

--- exam/Program.cs
+++ exam/Program.cs
@@ -2,3 +2,17 @@
 //либо равна 3 символам. 
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Console.WriteLine("Введите количество элементов: ");
+int num = int.Parse(Console.ReadLine());
+string[] arraystr(int size)
+{
+    string [] array=new  string [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент: ");
+        array[i]=Console.ReadLine();
+    }
+    return array;
+}
+
+string [] mass=arraystr(num);
